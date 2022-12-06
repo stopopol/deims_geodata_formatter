3e716633ee361061399be98abfe1095550e338d2
stopopol <christoph.wohner@umweltbundesamt.at>
2022-12-06 09:30:45 +0100

    Update deims_geodata_formatter.js

--- js/deims_geodata_formatter.js
+++ js/deims_geodata_formatter.js
@@ -29,7 +29,7 @@
 					layers: []
 				});
 				var layerControl = L.control.layers().addTo(map);
-				
+				L.control.scale().addTo(map);
 				L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 					maxZoom: 19,
 					attribution: '© OpenStreetMap'
